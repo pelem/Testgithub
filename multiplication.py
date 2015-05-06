3f59387004e3ebdcb8d497141af176273b806580
Mickaël Pelé <mickael.pele@ujf-grenoble.f>
2015-05-06 07:49:03 +0200

    Utilisation d'une boucle for

--- multiplication.py
+++ multiplication.py
@@ -1,13 +1,2 @@
-print(7 * 0)
-print(7 * 1)
-print(7 * 2)
-print(7 * 3)
-print(7 * 4)
-print(7 * 5)
-print(7 * 6)
-print(7 * 7)
-print(7 * 8)
-print(7 * 9)
-print(7 * 10)
-
-
+for i in range(11):
+	print(7 * i)
